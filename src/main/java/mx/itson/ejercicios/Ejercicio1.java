09276eb3f8cb34581562f86b297931b78d626794
ImLuisAlvarado <Luisalvarado00z22@gmail.com>
2024-08-28 20:17:04 -0700

    Finished the first and second excercise. Goodnight ;)

--- src/main/java/mx/itson/ejercicios/Ejercicio1.java
+++ src/main/java/mx/itson/ejercicios/Ejercicio1.java
@@ -4,6 +4,8 @@
 
 package mx.itson.ejercicios;
 
+import java.util.Scanner;
+
 /**
  *
  * @author darkheaven
@@ -11,6 +13,39 @@ package mx.itson.ejercicios;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        while (true){ 
+            
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("Ingrese una palabra: ");
+        String palabra = sc.nextLine();
+        
+        String palabraInversa = "";
+        
+        for(int i = palabra.length() - 1; i >= 0; i--) {
+	palabraInversa = palabraInversa + palabra.charAt(i);
+        }
+        
+        if (palabra.toLowerCase().equals(palabraInversa.toLowerCase())){
+            System.out.println("La palabra "+palabra+" si es un palindromo");
+        }
+        else{
+            System.out.println("La palabra "+palabra+" no es un palindromo");
+        }
+        
+        System.out.println("¿Desea intentar con otra palabra? (s/n)");
+        String respuesta = sc.nextLine();
+        
+        if (respuesta.toLowerCase().equals("s")){
+        }
+        else{
+            break;
+        }
+        }        
     }
 }
+
+        
+        
+        
--- src/main/java/mx/itson/ejercicios/Ejercicio2.java
+++ src/main/java/mx/itson/ejercicios/Ejercicio2.java
@@ -4,10 +4,40 @@
  */
 package mx.itson.ejercicios;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 /**
  *
  * @author darkheaven
  */
 public class Ejercicio2 {
+    public static void main(String[] args) {
     
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        List<String> listaNumeros = new ArrayList<>();
+
+        while (true) {
+            listaNumeros.clear();
+
+            for (int i = 0; i < 10; i++) {
+                int numero = random.nextInt(100);
+                listaNumeros.add(String.valueOf(numero));
+            }
+
+            for (String n : listaNumeros) {
+                System.out.println(n);
+            }
+
+            System.out.println("¿Desea terminar el programa? (s/n)");
+            String respuesta = sc.nextLine();
+
+            if (respuesta.toLowerCase().equals("s")) {
+                break;
+            }
+        }
+    }
 }
