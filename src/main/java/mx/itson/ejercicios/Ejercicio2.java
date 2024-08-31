73b553554fdfa9e2cdf6698f0348230a4d8dd9a0
ImLuisAlvarado <Luisalvarado00z22@gmail.com>
2024-08-30 18:03:51 -0700

    All finished. Tha-tha-tha-that's all folks! ;) Added missing parts of the second excercise.

--- src/main/java/mx/itson/ejercicios/Ejercicio2.java
+++ src/main/java/mx/itson/ejercicios/Ejercicio2.java
@@ -18,20 +18,62 @@ public class Ejercicio2 {
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
 
-        List<String> listaNumeros = new ArrayList<>();
+        List<Integer> listaNumeros = new ArrayList<>();
+        List<Integer> listaNumPrimos = new ArrayList<>();
+        List<Integer> listaNumPares = new ArrayList<>();
 
         while (true) {
             listaNumeros.clear();
+            listaNumPrimos.clear();
+            listaNumPares.clear();
 
             for (int i = 0; i < 10; i++) {
                 int numero = random.nextInt(100);
-                listaNumeros.add(String.valueOf(numero));
+                listaNumeros.add(Integer.valueOf(numero));
             }
-
-            for (String n : listaNumeros) {
-                System.out.println(n);
+            int numeroMayor = listaNumeros.getFirst();
+            int numeroMenor = listaNumeros.getFirst();
+            int promedio = 0;
+            int iteraciones = 0;
+            
+            for (int n : listaNumeros) {
+                
+                if(n % 2 == 0){
+                    listaNumPares.add(n);
+                }
+                
+                boolean esPrimo = true;
+                if (n < 2) {
+                    esPrimo = false;
+                } else {
+                    for (int i = 2; i * i <= n; i++) {
+                        if (n % i == 0) {
+                            esPrimo = false;
+                            break;
+                        }
+                    }
+                }
+                if (esPrimo) {
+                    listaNumPrimos.add(n);
+                }
+                
+                if (n>numeroMayor){
+                    numeroMayor = n;
+                }
+                if (n<numeroMenor){
+                    numeroMenor = n;
+                }
+            iteraciones++;
+            promedio += n;
             }
-
+            System.out.println("Lista de números generados:");
+            System.out.println(listaNumeros);
+            System.out.println("Promedio: " + (promedio / iteraciones));
+            System.out.println("Números primos generados: " + listaNumPrimos);
+            System.out.println("Números pares generados: " + listaNumPares);
+            System.out.println("Número mayor: " + numeroMayor);
+            System.out.println("Número menor: " + numeroMenor);
+            
             System.out.println("¿Desea terminar el programa? (s/n)");
             String respuesta = sc.nextLine();
 
